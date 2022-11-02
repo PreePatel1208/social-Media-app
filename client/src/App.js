89e88d9ecb16a70f23873bab5010b343043a38ab
preet <patelpreet0812@gmail.com>
2022-11-02 18:12:36 +0530

    worked on exception handler

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,51 @@
 import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import { Container } from "semantic-ui-react";
-
 import { AuthProvider } from "./context/auth";
 import AuthRoute from "./utils/AuthRoute";
-
 import "semantic-ui-css/semantic.min.css";
-import "./App.css";
-
 import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SinglePost from "./pages/SinglePost";
-import { AuthContext } from "./context/auth";
+import { ErrorBoundary } from 'react-error-boundary'
+import ExceptionHandler, { BuggyCounter } from "./components/ExceptionHandler";
+import "./App.css";
+
+
 
 function App() {
-   
+
+    function ErrorFallback({ error }) {
+        return (
+            <div role="alert">
+                <p>Something went wrong:</p>
+                <pre style={{ color: 'red' }}>{error.message}</pre>
+            </div>
+        )
+    }
+
     return (
-        <AuthProvider>
-            <Router>
-                <Container>
-                    <MenuBar />
-                   <Route exact path="/" component={Home} />
-                    <AuthRoute exact path="/login" component={Login} />
-                    <AuthRoute exact path="/register" component={Register} />
-                    <Route exact path="/posts/:postId" component={SinglePost} />
-                </Container>
-            </Router>
-        </AuthProvider>
-    );
+        <>
+            {/* <ExceptionHandler/> */}
+            {/* <h2>react error boundry</h2> */}
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <AuthProvider>
+                    <Router>
+                        <Container>
+                            <MenuBar />
+                            <Route exact path="/" component={Home} />
+                            <AuthRoute exact path="/login" component={Login} />
+                            <AuthRoute exact path="/register" component={Register} />
+                            <Route exact path="/posts/:postId" component={SinglePost} />
+                        </Container>
+                    </Router>
+                </AuthProvider>
+            </ErrorBoundary>
+        </>
+    )
+
 }
 
 export default App;
